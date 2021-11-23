f3297633ef4ebdb690afb402a85b9f1f770ef270
SIM9N <91584856+SIM9N@users.noreply.github.com>
2021-11-23 18:41:59 +0800

    Add files via upload

--- IR.c
+++ IR.c
@@ -1,53 +1,83 @@
 #include "IR.h"
 
-//testing the IR for High and Low
-
-
-//Transmitting Code
-
-/*This function turns off IR_LED and delays for 12us*/
-void send_low(void) {
-	HAL_GPIO_WritePin(GPIOB, IR_Pin, GPIO_PIN_RESET); //turn off IR_LED
-	DWT_Delay(IR_DELAY);
+void delay_us(uint32_t i)
+{
+    uint32_t temp;
+    SysTick->LOAD=9*i;         //Set the reinstallation value, at 72MHZ
+    SysTick->CTRL=0X01;         //Enable, reduce to zero is no action, use external clock source
+    SysTick->VAL=0;                //Clear counter
+    do
+    {
+        temp=SysTick->CTRL;           //Read the current countdown value
+    }
+    while((temp&0x01)&&(!(temp&(1<<16))));     //Waiting time arrives
+    SysTick->CTRL=0;    //Turn off the counter
+    SysTick->VAL=0;        //Clear counter
 }
-/*This function turns on IR_LED and delays for 12us*/
-void send_high(void) {
-	HAL_GPIO_WritePin(GPIOB, IR_Pin, GPIO_PIN_SET); //turn on IR_LED
-	DWT_Delay(IR_DELAY);
-}
-/*This function sends IR bursts for time specified, in microseconds*/
-void burst(int length) {
-	while (length > 0) {
-		send_high();
-		send_low();
-		length -= IR_DELAY * 2;
+
+uint16_t countHighTime(){
+	uint16_t counter = 0;
+	char buf[10];
+	while(HAL_GPIO_ReadPin(IRSensor_GPIO_Port, IRSensor_Pin)){
+		counter++;
+		delay_us(1);
+		if (counter > 5000)
+			return 5000;
+			//LCD_DrawString(0,300,"error");
 	}
-	HAL_GPIO_WritePin(GPIOB, IR_Pin, GPIO_PIN_RESET); //turn off IR_LED
+	return counter;
 }
 
-void send_leading_code(void){
-	burst(9000000);
-	DWT_Delay(4500000);
-}
-
-/*This function sends the code specified.
-1000us bursts are translated to logical 0s.
-1500us bursts are translated to logical 1s.
-This code and function is unique to each gun.*/
-void send_code(void) { //sends 0b00101010 -> 42
-	send_leading_code();
-	burst(1500); DWT_Delay(500);
-	burst(1500); DWT_Delay(500);
-	burst(1000); DWT_Delay(500);
-	burst(1000); DWT_Delay(500);
-	burst(1000); DWT_Delay(500);
-	burst(1500); DWT_Delay(500);
-	burst(1500); DWT_Delay(500);
-	burst(1500); DWT_Delay(500);
-}
-/*This function sends your code multiple times via the IR_LED.*/
-void fire_blaster(uint8_t times) {
-	for (uint8_t i = 0; i < times; i++) {
-		send_code();
+/*
+void decode(uint16_t *data)
+{
+	char buf[50];
+	*data = 0;
+	uint16_t counter = 0;
+	uint32_t countExit = 0;
+	LCD_DrawString(0, 0, "inside decode");
+	//check for the 4.5ms low time 4500us
+	do{
+			counter = 0;
+			counter = countHighTime();
+			countExit++;
+			if (countExit > 10000){
+				return;
+				LCD_DrawString(0, 0, "returned");
+			}		
+	}while(counter < 100);
+	
+	sprintf(buf,"%d",counter);
+	LCD_DrawString(200, 0, buf);
+	
+	if(counter < 2000 || counter > 6000)
+		return;
+	
+	LCD_DrawString(0, 0+HEIGHT_EN_CHAR, "4.5ms detected");
+	countExit =0;
+	
+	//state decoding
+	for (int i=0; i<16; i++){
+		//ignoring the small pulse within 12us
+		do{
+			counter = 0;
+			counter = countHighTime();
+			countExit++;
+			if (countExit > 2000)
+				return;
+		}while(counter <50);
+	
+		// HighTime [300,900]   ->   0
+		if (counter > 300 && counter < 900){
+			data += 0<<(16-i);
+			LCD_DrawString(0, 2*HEIGHT_EN_CHAR+HEIGHT_EN_CHAR *i, "0");
+		}
+			
+		// HighTime [900,1500]  ->   1
+		if (counter > 900 && counter < 1500){
+			data += 1<<(16-i);
+			LCD_DrawString(0, 2*HEIGHT_EN_CHAR+HEIGHT_EN_CHAR *i, "1");
+		}
 	}
 }
+*/
