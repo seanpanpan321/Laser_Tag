c64c57bcca4acbe4c6303117aec9f87ad9f3671e
SIM9N <91584856+SIM9N@users.noreply.github.com>
2021-11-23 18:41:44 +0800

    Add files via upload

--- IR.h
+++ IR.h
@@ -2,20 +2,11 @@
 #define IR_H
 
 #include "main.h"
-#include "dwt_delay.h"
 #include <stdint.h>
 #include "stm32f1xx_hal_gpio.h"
-
-#define IR_LED 0
-#define IR_DELAY 12
-#define BITS_PER_CODE 8
-//TransmittingFunctions
-void send_low();
-void send_high();
-void burst(int length);
-void send_leading_code(void);
-void send_code(void);
-void fire_blaster(uint8_t times);
+#include "lcd.h"
+void delay_us(uint32_t i);
+uint16_t countHighTime();
 
 
 #endif
